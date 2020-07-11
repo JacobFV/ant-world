9e9861e4fff16d94519b37e7f51ac7b81409e237
Jacob Valdez <jacobfv@msn.com>
2020-07-11 18:02:04 -0500

    about to build wheels

--- setup.py
+++ setup.py
@@ -1,1 +1,28 @@
-# TODO
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='smae',
+    version='0.0.1',
+    license="MIT",
+    url="https://github.com/JacobFV/smae",
+    description='Social Multi-Agent Environment',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Jacob Valdez',
+    author_email='jacobfv@msn.com',
+    packages=['smae'],  #same as name
+    install_requires=[
+        'numpy==1.19.0',
+        'tensorflow==2.2.0',
+        'gym==0.17.2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
